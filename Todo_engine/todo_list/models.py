0101b6b4dacec5907cfa2fe5d22c30f8f12102bd
valid <valid.saley@gmail.com>
2020-02-08 20:30:57 +0300

    write first model

--- Todo_engine/todo_list/models.py
+++ Todo_engine/todo_list/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    check = models.BooleanField()
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    position = models.AutoField(unique=True)
